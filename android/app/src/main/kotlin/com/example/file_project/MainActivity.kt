d24f23289eb03c1904ddcc9f8cd4f7b32970c288
ducjk <duchuynh360@gmail.com>
2022-11-08 22:55:52 +0700

    push code

---
+++ android/app/src/main/kotlin/com/example/file_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.file_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/file/file.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class CounterFile {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    print(path);
+
+    return File('$path/counter.txt');
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    return file.writeAsString('$counter');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      final contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+}
